4c2592688b4701c11da0b42540452f2c66630384
CDDing <jangmee9918@gmail.com>
2023-09-19 20:13:22 +0900

    OpenGL 초기화

--- Graduation_Project/kernel.cu
+++ Graduation_Project/kernel.cu
@@ -1,92 +1,85 @@
 ﻿
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include <glad/glad.h>
+#include <GL/glew.h>
 #include <cuda_gl_interop.h>
-#include <GLFW/glfw3.h>
+#include <GL/glut.h>
 #include <stdio.h>
+#include <windows.h>
 int windowWidth = 800, windowHeight = 800;
+
+GLuint pbo_dest;
+struct cudaGraphicsResource* cuda_pbo_dest_resource;
+unsigned int size_tex_data;
+unsigned int num_texels;
+unsigned int num_values;
+unsigned int image_width = 800;
+unsigned int image_height = 800;
+void createPBO(GLuint *pbo, struct cudaGraphicsResource **pbo_resource) {
+
+    num_texels = image_width * image_height;
+    num_values = num_texels * 4;
+    size_tex_data = sizeof(GLubyte) * num_values;
+    void* data = malloc(size_tex_data);
+
+    //예제에서는 GL_ARRAY_BUFFER인데 GL_PIXEL_UNPACK_BUFFER가 텍스쳐를 담는다고 함. 알아봐야될듯
+    glGenBuffers(1, pbo);
+    glBindBuffer(GL_ARRAY_BUFFER, *pbo);
+    glBufferData(GL_ARRAY_BUFFER, size_tex_data, data, GL_DYNAMIC_DRAW);
+    free(data);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    cudaGraphicsGLRegisterBuffer(pbo_resource, *pbo, cudaGraphicsMapFlagsNone);
+
+
+}
+void deletePBO(GLuint* pbo) {
+    glDeleteBuffers(1, pbo);
+    *pbo = 0;
+}
+
+
+
+
+
+
+
+
+
 
 
 //__global__ void update_frame(cudaSurfaceObject_t surface, int max_x, int max_y, int ns, curandState* rand_state, float t) {
 
 //}
 
+void renderScene() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
 
-static void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error: %s\n", description);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-    else if (key == GLFW_KEY_A) {
-        //    camera_location = camera_location - vec3(0.1f, 0.0f, 0.0f);
-    }
-    else if (key == GLFW_KEY_W) {
-        printf("A눌림");
-        //      camera_location = camera_location - vec3(0.0f, 0.0f, 0.1f);
-    }
-    else if (key == GLFW_KEY_S) {
-        printf("A눌림");
-        //    camera_location = camera_location + vec3(0.0f, 0.0f, 0.1f);
-    }
-    else if (key == GLFW_KEY_D) {
-        printf("A눌림");
-        //     camera_location = camera_location + vec3(0.1f, 0.0f, 0.0f);
-    }
-}
-static void resize_callback(GLFWwindow* window, int new_width, int new_height) {
-    glViewport(0, 0, windowWidth = new_width, windowHeight = new_height);
+void initGL() {
+
+    glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE);
+    glutInitWindowSize(windowWidth, windowHeight);
+    glutCreateWindow("CType Ray Tracing");
+    glDisable(GL_DEPTH_TEST);
+
+    glViewport(0, 0, windowWidth, windowHeight);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, windowWidth, windowHeight, 0.0, 0.0, 100.0);
+    glOrtho(0.0, windowWidth, windowHeight, 0, 0, 1);
     glMatrixMode(GL_MODELVIEW);
+    glClearColor(0.5, 0.5, 0.5, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glutDisplayFunc(renderScene);
+    glutMainLoop();
 }
 
-
-int main()
+int main(int argc, char** argv)
 {
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-
-
-    if (!glfwInit()) {
-        return -1;
-    }
-
-    window = glfwCreateWindow(windowWidth, windowHeight, "CType Ray Tracing", NULL, NULL);
-
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetWindowSizeCallback(window, resize_callback);
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGL()) {
-        printf("GLAD 초기화 실패");
-        return -1;
-    }
-
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    while (!glfwWindowShouldClose(window)) {
-
-        //cudaDeviceSynchronize(); //쿠다 연산이 완료될 때 까지 기다리는 함수.
-        glClear(GL_COLOR_BUFFER_BIT);
-        glEnable(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, textureID);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    glutInit(&argc, argv);
+    initGL();
     return 0;
 }
