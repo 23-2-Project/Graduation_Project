c1610042a381f3c0c9edf18514247cc4ffa9ffbb
CDDing <jangmee9918@gmail.com>
2023-09-19 17:02:31 +0900

    OpenGL 설정

--- Graduation_Project/kernel.cu
+++ Graduation_Project/kernel.cu
@@ -1,7 +1,7 @@
 ﻿
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include <glad/glad.h>
 #include <stdio.h>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
